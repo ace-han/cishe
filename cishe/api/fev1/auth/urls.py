ca5a4175d81637dc64df252459c2cccc1ffc7988
Ace <ace.jl.han@gmail.com>
2020-09-01 00:32:50 +0800

    ref #47, #48, helper function views for permission httponly cookie

--- cishe/api/fev1/auth/urls.py
+++ cishe/api/fev1/auth/urls.py
@@ -4,6 +4,9 @@ from cishe.api.fev1.auth.views import (
     TokenCookieDeleteView,
     TokenObtainPairView,
     TokenRefreshView,
+    token_401,
+    token_403,
+    unauthorized_403,
 )
 
 
@@ -11,4 +14,7 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/delete/", TokenCookieDeleteView.as_view(), name="token_delete"),
+    path("token/token-403/", token_403, name="token_403"),
+    path("token/unauthorized/", unauthorized_403, name="unauthorized_403"),
+    path("token/token-401/", token_401, name="token_401"),
 ]
--- cishe/api/fev1/auth/views.py
+++ cishe/api/fev1/auth/views.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import (
     TokenCookieDeleteView as OriginalTokenCookieDeleteView,
@@ -58,3 +60,30 @@ class TokenRefreshView(OriginalTokenRefreshView):
 
 class TokenCookieDeleteView(OriginalTokenCookieDeleteView):
     token_refresh_view_name = "api:fev1:auth:token_refresh"
+
+
+@api_view(["GET", "POST"])
+def token_403(request):
+    return Response(
+        {
+            "detail": "Given token not valid for any token type",
+            "code": "token_not_valid",
+        },
+        status=HTTP_403_FORBIDDEN,
+    )
+
+
+@api_view(["GET", "POST"])
+def unauthorized_403(request):
+    return Response(
+        {"detail": "You do not have permission to perform this action."},
+        status=HTTP_403_FORBIDDEN,
+    )
+
+
+@api_view(["GET", "POST"])
+def token_401(request):
+    return Response(
+        {"detail": "Token is invalid or expired.", "code": "token_not_valid"},
+        status=HTTP_401_UNAUTHORIZED,
+    )
