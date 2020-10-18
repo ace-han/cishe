3b4a06d14f16b38b76681ffcbe8b8d167531d77f
Ace <ace.jl.han@gmail.com>
2020-10-18 23:40:56 +0800

    enrich contract serializer

--- cishe/api/fev1/contract/serializers.py
+++ cishe/api/fev1/contract/serializers.py
@@ -4,12 +4,6 @@ from rest_framework.serializers import ALL_FIELDS
 from cishe.contract.models import Contract, Customer, ServiceInfo, TakeOver
 
 
-class ContractSerializer(FlexFieldsModelSerializer):
-    class Meta:
-        model = Contract
-        fields = ALL_FIELDS
-
-
 class CustomerSerializer(FlexFieldsModelSerializer):
     class Meta:
         model = Customer
@@ -20,6 +14,17 @@ class ServiceInfoSerializer(FlexFieldsModelSerializer):
     class Meta:
         model = ServiceInfo
         fields = ALL_FIELDS
+
+
+class ContractSerializer(FlexFieldsModelSerializer):
+    class Meta:
+        model = Contract
+        fields = ALL_FIELDS
+
+    expandable_fields = {
+        "customer": (CustomerSerializer, {"source": "customer"}),
+        "serviceinfo": (ServiceInfoSerializer, {"source": "serviceinfo"}),
+    }
 
 
 class TakeOverSerializer(FlexFieldsModelSerializer):
